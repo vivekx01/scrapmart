4206d91949bfd1642ee774e1dad62f258f6f00f2
Vivek <72996825+vivekx01@users.noreply.github.com>
2021-07-07 18:47:12 +0530

    Update settings.py

--- Scrapmart/settings.py
+++ Scrapmart/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k7v8j@m#g+s)vn&(5lr(3h8lj20*-@7#9^e^a2o%0&61*&ngbf'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
