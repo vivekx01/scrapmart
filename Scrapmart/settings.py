9d0de989c29b8eda2b3c73af4a35c0940436e754
Vivek Shukla <viveks95944@gmail.com>
2021-07-18 22:15:35 +0530

    final2

--- Scrapmart/settings.py
+++ Scrapmart/settings.py
@@ -21,16 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
-
+SECRET_KEY = '@_l=j9i8&!)zi)0le!iq^um5ndgph8-ke-zs2=41#(-kj02+v+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['scrapmart.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
-SECRET_KEY = '@_l=j9i8&!)zi)0le!iq^um5ndgph8-ke-zs2=41#(-kj02+v+'
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
