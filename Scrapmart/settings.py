0c13bb3a73c4b4d6e738d6a42cb2a7c6395fc70c
Vivek Shukla <viveks95944@gmail.com>
2021-07-18 22:23:07 +0530

    final5

--- Scrapmart/settings.py
+++ Scrapmart/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '@_l=j9i8&!)zi)0le!iq^um5ndgph8-ke-zs2=41#(-kj02+v+'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
